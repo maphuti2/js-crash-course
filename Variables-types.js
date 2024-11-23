131f3b9b0bf5ac26ddcd3f611c9ddd54ebddf1f6
maphuti2 <maphuti22r@gmail.com>
2024-11-23 10:36:37 +0200

    Variables & datatypes

--- Variables-types.js
+++ Variables-types.js
@@ -1,1 +1,35 @@
-alert("chech 1221");
+// ways to show your Output on the browser
+
+//alert("chech 1221"); // message box
+document.write("hey!"); // inside html file
+console.log("from console"); // browser console and it has methods
+//console.warn("hey...");
+//console.error("not this again");
+console.info("Am here...");
+
+
+// Declaring variables we use let & const
+let age = 22;
+age = 25; // with let i can change values of variables
+console.log(age);
+
+const name = "Maphuti";  // with const you cannot change it
+console.log(name);
+
+// Data Types
+// String, Number, Boolean, Undefined, symbols, Objects
+
+const fruit = "Apple";
+const score = 30.2;
+const invites = 20;
+const isPass = false;
+const x = null;
+const y = undefined;
+
+console.log("------------------------------------");
+console.log(typeof fruit);
+console.log(typeof score);
+console.log(typeof invites);
+console.log(typeof isPass);
+console.log(typeof x);
+console.log(typeof y);
