55ea966081bbcba2b3797dad5da95a5205fa6f9c
maphuti2 <maphuti22r@gmail.com>
2024-11-23 11:01:45 +0200

    Learning string format

---
+++ strings.js
@@ -0,0 +1,11 @@
+//Strings can be created using single quotes, double quotes & backticks
+let singleQuote = 'Fede Dah';
+let doubleQuote = "Fede Ola";
+let backtick = `Hey everyone`;
+
+// output multiple strings in one line
+
+console.log("show 1st string: " + singleQuote + " & 2nd string: " + doubleQuote);
+// template string
+console.log(`Thabo: ${backtick}. 
+mike: ${singleQuote}`);
