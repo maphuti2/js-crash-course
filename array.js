d811c094735aad70d5d47123a9cb827317834dfb
maphuti2 <maphuti22r@gmail.com>
2024-11-25 22:46:45 +0200

    creating array

--- array.js
+++ array.js
@@ -1,1 +1,8 @@
-console.log("trrr...")
+// Arrays - are variables that hold multiple values
+
+const greet = ['hi', 'hello', 'hey'];
+
+console.log(greet) 
+greet[1];       //displays specific value inside an array
+greet.push('howzit');       //adds value inside an array at the end
+console.log(greet);
