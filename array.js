6b76e9c57498d4e445cc8f75faa0e29f1021f45d
maphuti2 <maphuti22r@gmail.com>
2024-11-25 22:59:06 +0200

    Few Array Methods

--- array.js
+++ array.js
@@ -6,3 +6,9 @@ console.log(greet)
 greet[1];       //displays specific value inside an array
 greet.push('howzit');       //adds value inside an array at the end
 console.log(greet);
+greet[0] = 'Hi';
+greet.pop();        // removes last array element
+greet.unshift('morning');    //adds value in the beginning of an array   
+console.log(greet)      //modifying array element
+
+console.log(Array.isArray(greet));
