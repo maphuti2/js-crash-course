0d6e413588462dc3502c50977412b32bcdd09a79
maphuti2 <maphuti22r@gmail.com>
2024-11-25 22:30:14 +0200

    String Methods

--- strings.js
+++ strings.js
@@ -9,3 +9,14 @@ console.log("show 1st string: " + singleQuote + " & 2nd string: " + doubleQuote)
 // template string
 console.log(`Thabo: ${backtick}. 
 mike: ${singleQuote}`);
+
+// String Methods
+
+const myString = 'Javascript is Good!';
+
+console.log(myString.length); // displays mumber of characters
+console.log(myString.toLowerCase());   // displays lower case 
+console.log(myString.toUpperCase());    // displays Upper cases
+console.log(myString.substring(0,10));      //displays part of characters u want from a string
+console.log(myString.split(' '));        //convert string into an array
+
