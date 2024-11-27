98db26d4a791ee034b61a9ea50b3aab74768d351
maphuti2 <maphuti22r@gmail.com>
2024-11-27 10:20:39 +0200

    Printing object from an array

--- object.js
+++ object.js
@@ -1,5 +1,41 @@
+// Creating Object 
 const cars = {name: 'Kia',
     model: 'KZ-2036',
+    brand: 12,
+    color: ['yellow', 'red', 'black', 'white'],
+    availability: {
+        model: 'ka-2002',
+        city: 'polokwane',
+    },
 }
 
-console.log(cars.model);
+console.log(cars.model, cars.color[1], cars.availability.city);        // Displaying Object
+
+const {name, model, color, availability: {city, } } = cars;     // constructor format
+
+console.log(city, color[2]);
+
+cars.country = 'South Africa';
+console.log(cars)
+
+// Working with array that containers objects
+
+const chaos = [{
+    id: 1,
+    task: "clean Dishes",
+    isFinished: false
+},
+{id: 2,
+    task: "Clean House",
+    isFinished: false,
+},
+{id: 3,
+    task: "make Coffee",
+    isFinished: false,
+},
+{id: 4,
+    task: "Practice coding",
+    isFinished: false,
+}];
+
+console.log(chaos[3].task)
