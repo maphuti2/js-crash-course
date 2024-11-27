604a16bfdd222274212cd8cb1466f980684b096f
maphuti2 <maphuti22r@gmail.com>
2024-11-27 18:27:55 +0200

    if statement

--- conditionals.js
+++ conditionals.js
@@ -1,2 +1,13 @@
 // Conditionals statement 
 
+// if statement
+
+let x = 2;
+if (x === 0){
+    console.log("Lucky You!");
+}else if (x === 5){
+    console.log("It's a five!");
+}
+else{
+    console.log("Your Wrong!");
+}
