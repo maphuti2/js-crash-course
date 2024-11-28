e765fe89d0c97b3237715109d348dffbf457c50c
maphuti2 <maphuti22r@gmail.com>
2024-11-29 01:22:19 +0200

    basic class structure

---
+++ class.js
@@ -0,0 +1,39 @@
+// Learning Classes
+
+// Creating Class or Defining a Class
+
+class Person {
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    // Method
+    getFullName(){
+        console.log(`${this.firstName} ${this.lastName} ${this.age}yrs`);
+    }
+}
+
+// Create an instance of the Person class
+
+const person1 = new Person('Maphuti', 'Makwela', '30');
+person1.getFullName();
+
+// Class Inheritance
+
+class Job extends Person {
+    constructor(firstName, lastName, age, jobTitle){
+        super(firstName,lastName,age);
+        this.jobTitle = jobTitle;
+    }
+
+    employeeDetail(){
+        console.log(`${this.firstName} ${this.lastName} ${this.age}yrs works as ${this.jobTitle}`);
+    }
+}
+
+const employee1 = new Job('Thapelo', 'Mamo', 26, 'Software Developer');
+
+employee1.employeeDetail();
+
