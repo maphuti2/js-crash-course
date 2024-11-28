db2444bbcd5028a48d51f6c0d23d4aebf2c03c90
maphuti2 <maphuti22r@gmail.com>
2024-11-28 23:55:08 +0200

    arrow function

--- function.js
+++ function.js
@@ -4,4 +4,12 @@ function greet(){
     console.log("hi, people!")
 }
 
-console.log(greet());
+greet();
+
+// Arrow function
+
+const addNum = (num1, num2) =>{
+    return num1 + num2;
+}
+
+console.log(addNum(7,1));
