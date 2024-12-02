9e7195ca88e83ef918cc0aecccadd5ba24fca67c
maphuti2 <maphuti22r@gmail.com>
2024-12-02 17:50:31 +0200

    Few things about DOM

--- dom.js
+++ dom.js
@@ -1,1 +1,31 @@
-alert(200);
+// Document Object Model (DOM) is a tree-like structure where each node represents a part of the document.
+
+
+// DOM Selector
+//Single selection
+document.getElementById('my-form');
+document.querySelector('name');
+
+// Multiple selector
+
+document.querySelectorAll('.msg');
+
+// Accessing the element
+
+const getElement = document.getElementById('content');
+console.log(getElement);
+
+// Creating new element
+
+const newPara = document.createElement('p');
+newPara.textContent = 'What else can I do?';
+
+// Appending/ Adding new element
+
+getElement.appendChild(newPara);
+
+const b= document.querySelector('.btn');
+
+b.style.background = 'blue';
+
+b.addEventListener('click', () => alert('Xo Dah!'));
