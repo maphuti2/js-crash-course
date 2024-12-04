ddc3e80cce027aacf623e4456d3ff9b4fbd071ab
maphuti2 <maphuti22r@gmail.com>
2024-12-04 05:53:45 +0200

    Fixing callback function

--- callbacks.js
+++ callbacks.js
@@ -3,15 +3,24 @@ const posts = [
     {title: 'Post Two', body: 'This is post two'},
 ];
 
+// function to get post
 function getPosts(){
     setTimeout(() => {
         let output = '';
-        posts.forEach((post, index) => {
+        posts.forEach((post , index) =>{
             output += `<li>${post.title}</li>`;
         });
-
-        document.body.textContent = output;
-    }, 3000);
+        document.body.innerHTML = output;
+    }, 2000);
 }
 
-getPosts();
+// function that creates posts
+
+function createPosts(post, callback){
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    },3000)
+}
+
+createPosts({title: 'Post Three', body: 'This is post three.'}, getPosts);
