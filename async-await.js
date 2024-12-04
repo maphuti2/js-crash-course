145b2132e767fe15822d9306bab9fbddd2aa70a2
maphuti2 <maphuti22r@gmail.com>
2024-12-04 07:22:57 +0200

    async function

--- async-await.js
+++ async-await.js
@@ -1,2 +1,45 @@
 // Async / await
 
+const posts = [
+    {title: 'Post one', body: 'This is post one'},
+    {title: 'Post Two', body: 'This is post two'},
+];
+
+// Adding post
+function myPosts(){
+    let output = '';
+    setTimeout(() =>{
+        posts.forEach((post) =>{
+            output += `<li>${post.title}</li>`
+        });
+
+        document.body.innerHTML = output;
+    },2000);
+}
+
+// creating post
+
+function createPost(post){
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            posts.push(post);
+
+            const err = false;
+
+            if(!err){
+                resolve();
+            }else{
+                reject('Error happened!');
+            }
+    
+        }, 3000);
+    })
+    
+}
+async function init() {
+    await createPost({title: 'post 3', body: 'This 3'});
+
+    myPosts();
+}
+
+init();
