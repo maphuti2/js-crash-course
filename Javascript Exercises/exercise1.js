6664f35f95b8d99d580cddcee115486260f0ee19
maphuti2 <maphuti22r@gmail.com>
2024-12-10 13:06:27 +0200

    Challenge 3 &4 completed

--- Javascript Exercises/exercise1.js
+++ Javascript Exercises/exercise1.js
@@ -31,3 +31,56 @@ const extension = (file) => {
 }
 
 extension('k.i.pdf');
+
+
+/*
+Challenge 3
+-----------  
+
+Write a JavaScript program to replace every character in a given string with the character following it in the alphabet
+
+*/
+
+const nextLetters = (name) => {
+    let result = '';
+
+    for (let a =0; a < name.length; a++){
+        let nextL = String.fromCharCode(name.charCodeAt(a)+1);
+        result += nextL;
+    }
+
+    return result;
+}
+
+nextLetters('abc');
+
+// Simple but more nerdy aproach
+
+const nextChar = (str) => {
+    return str.split('').map(char => String.fromCharCode(char.charCodeAt(0) + 1)).join('');
+}
+
+nextChar('ab');
+
+//********************************************************** */
+
+/* 
+    Challenge 4
+    ------------
+
+    Write a JavaScript program to get the current date.
+    Expected Output :
+    mm-dd-yyyy, mm/dd/yyyy, dd-mm-yyyy or dd/mm/yyyy
+
+*/
+
+const currentDate = () => {
+    let whichDay = new Date();
+
+    const day = whichDay.getDate();
+    const month = whichDay.getMonth();
+    const year = whichDay.getFullYear();
+
+    return `${day}-${month}-${year}`;
+}
+currentDate();
